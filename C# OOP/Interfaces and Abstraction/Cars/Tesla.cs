a6631a1fb837c5284a2832efd6f92e6952081e7f
DimitarSamarov07 <samarovdimitar@gmail.com>
2019-07-16 17:38:11 +0300

    2/12 problems done!

---
+++ C# OOP/Interfaces and Abstraction/Cars/Tesla.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla:ICar,IElectricCar
+    {
+        private int battery;
+        private string color;
+        private string model;
+
+        public Tesla(string model,string color,int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+
+        public string Model
+        {
+            get => model;
+            set => model = value;
+        }
+
+        public string Color
+        {
+            get => color;
+            set => color = value;
+        }
+
+        public int Battery
+        {
+            get => battery;
+            set => battery = value;
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            return $"{Color} Tesla {Model} with {Battery} Batteries";
+        }
+    }
+}

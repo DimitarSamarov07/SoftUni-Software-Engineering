1e88042832d163dfa2790d2da65481a30f9007b7
DimitarSamarov07 <samarovdimitar@gmail.com>
2019-07-17 19:58:41 +0300

    9/12 problems done!

---
+++ C# OOP/Interfaces and Abstraction/Food Shortage/Citizen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Food_Shortage
+{
+    public class Citizen:IBuyer
+    {
+        public Citizen(string name,int age,string id,DateTime birthDate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.BirthDate = birthDate;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Id { get; }
+        public int Food { get; set; }
+        public DateTime BirthDate { get; set; }
+        public void BuyFood()
+        {
+            Food += 10;
+        }
+
+        public int GetFoodAmount()
+        {
+            return this.Food;
+        }
+    }
+}
---
+++ C# OOP/Interfaces and Abstraction/Food Shortage/StartUp.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Schema;
+
+namespace Food_Shortage
+{
+    class StartUp
+    {
+        public static List<Citizen> Citizens;
+        public static List<Rebel> Rebels;
+        static void Main(string[] args)
+        {
+            Citizens = new List<Citizen>();
+            Rebels = new List<Rebel>();
+            int numberOfLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string[] line = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (line.Length == 4)
+                {
+                    string name = line[0];
+                    int age = int.Parse(line[1]);
+                    string id = line[2];
+                    int[] birthDate = line[3].Replace("/"," ").Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+                    DateTime date = new DateTime(birthDate[2], birthDate[1], birthDate[0]);
+                    Citizen citizen = new Citizen(name, age, id, date);
+                    Citizens.Add(citizen);
+                }
+                else if (line.Length == 3)
+                {
+                    string name = line[0];
+                    int age = int.Parse(line[1]);
+                    string group = line[2];
+                    Rebel rebel = new Rebel(name, age, group);
+                    Rebels.Add(rebel);
+                }
+            }
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (Citizens.Any(x => x.Name == input))
+                {
+                    List<Citizen> itemList = Citizens.Where(x => x.Name == input).ToList();
+                    Citizen item = itemList[0];
+                    int index = Citizens.IndexOf(item);
+                    item.Food += 10;
+                }
+                else if(Rebels.Any(x=>x.Name==input))
+                {
+                    List<Rebel> itemList = Rebels.Where(x => x.Name == input).ToList();
+                    Rebel item = itemList[0];
+                    int index = Rebels.IndexOf(item);
+                    item.Food += 5;
+                }
+               
+            }
+
+            Console.WriteLine(GetTotalFood());
+        }
+
+        private static string GetTotalFood()
+        {
+            int total = 0;
+            foreach (var citizen in Citizens)
+            {
+                total += citizen.GetFoodAmount();
+            }
+
+            foreach (var rebel in Rebels)
+            {
+                total += rebel.GetFoodAmount();
+            }
+
+            return total.ToString().TrimEnd();
+        }
+    }
+}

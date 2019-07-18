3025fc5d96a98181f20f387f585d9de105348411
DimitarSamarov07 <samarovdimitar@gmail.com>
2019-07-18 18:51:19 +0300

    10/12 problems done!

---
+++ C# OOP/Interfaces and Abstraction/Military Elite/Factories/PrivateFactory.cs
@@ -0,0 +1,36 @@
+﻿using _08MilitaryElite.Interfaces;
+using _08MilitaryElite.Models;
+
+namespace _08MilitaryElite.Factories
+{
+    public class PrivateFactory : SoldierFactory, ISoldier
+    {
+        public PrivateFactory(string id, string firstName, string lastName, decimal salary)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Salary = salary;
+        }
+
+        public string Id { get; private set; }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public decimal Salary { get; private set; }
+
+        //public Private CreatePrivate(string id, string firstName, string lastName, decimal salary)
+        //{
+        //    Private soldier = new Private(id, firstName, lastName, salary);
+
+        //    return soldier;
+        //}
+
+        public override Soldier GetSoldier()
+        {
+            return new Private(this.Id, this.FirstName, this.LastName, this.Salary);
+        }
+    }
+}

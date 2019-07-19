cbd1e2b8beaff6cc20afffe5ec80415e67eb49cd
DimitarSamarov07 <samarovdimitar@gmail.com>
2019-07-19 16:32:45 +0300

    80/100

---
+++ C# OOP/Polymorphism/Shapes/Rectangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+        private double Width
+        {
+            get => width;
+            set { width = value; }
+        }
+
+        private double Height
+        {
+            get => height;
+            set { height = value; }
+        }
+
+        public Rectangle(double width,double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+        public override double CalculatePerimeter()
+        {
+            return Width * 2 + Height * 2;
+
+        }
+
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + "Rectangle";
+        }
+    }
+}

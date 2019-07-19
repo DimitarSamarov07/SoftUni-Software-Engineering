f8a81132b9ac02c6ab708fdc453529ade1b3b48d
DimitarSamarov07 <samarovdimitar@gmail.com>
2019-07-19 15:26:15 +0300

    2/6 problems done!

---
+++ C# OOP/Polymorphism/Animals/Animal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string favouriteFood;
+        private string name;
+
+        public Animal(string name,string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string FavouriteFood
+        {
+            get => favouriteFood;
+            set => favouriteFood = value;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"I am {Name} and my fovourite food is {FavouriteFood}"+Environment.NewLine);
+            sb.Append("ANIMALLLLLS");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# OOP/Polymorphism/Animals/Dog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Dog:Animal
+    {
+        public Dog(string name, string favouriteFood)
+            : base(name, favouriteFood)
+        {
+        }
+
+        public override string ExplainSelf()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"I am {Name} and my fovourite food is {FavouriteFood}"+Environment.NewLine);
+            sb.Append("DJAAF");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

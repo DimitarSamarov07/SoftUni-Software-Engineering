9952f576247415ad8c1652980a53846877a79498
DimitarSamarov07 <samarovdimitar@gmail.com>
2019-07-22 13:32:25 +0300

    All problems done!!

---
+++ C# OOP/Polymorphism/Wild Farm/Models/Animal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wild_Farm.Core.Animals.Birds;
+using Wild_Farm.Core.Animals.Mammals.Felines;
+using Wild_Farm.Core.Food;
+
+namespace Wild_Farm.Core
+{
+    public abstract class Animal
+    {
+        public Animal(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+            Console.WriteLine(this.FoodAskSound);
+        }
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public int FoodEaten { get; set; }
+        public virtual List<string> WhatDoYouEat { get; set; } = new List<string>();
+        public virtual string FoodAskSound { get; set; } = "";
+        public virtual double WeightIncrease { get; set; } = 0;
+
+        public virtual void Feed(Food.Food food,string type)
+        {
+            if (!WhatDoYouEat.Contains(food.ToString()))
+            {
+                Console.WriteLine($"{type} does not eat {food}!");
+            }
+            else
+            {
+                FoodEaten += food.Quantity;
+                Weight+=WeightIncrease*food.Quantity;
+            }
+        }
+        public override string ToString()
+        {
+            string type = this.GetType().Name.ToString();
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{type} [{Name}, ");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# OOP/Polymorphism/Wild Farm/Models/Animals/Birds/Hen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wild_Farm.Core.Food;
+
+namespace Wild_Farm.Core.Animals.Birds
+{
+    public class Hen:Bird
+    {
+        public Hen(string name, double weight, double wingSize)
+            : base(name, weight, wingSize)
+        {
+        }
+        public override string FoodAskSound { get; set; } = "Cluck";
+        public override double WeightIncrease { get; set; } = 0.35;
+        public override List<string> WhatDoYouEat { get; set; }= new List<string>{"Vegetable","Seeds","Meat","Fruit"};
+    }
+}

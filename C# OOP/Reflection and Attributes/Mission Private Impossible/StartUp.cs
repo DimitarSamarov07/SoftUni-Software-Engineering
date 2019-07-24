6a675234c19a7a383c628232824fdbb2522b45e2
DimitarSamarov07 <samarovdimitar@gmail.com>
2019-07-24 11:24:12 +0300

    3/8 problems done!

---
+++ C# OOP/Reflection and Attributes/Mission Private Impossible/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.RevealPrivateMethods("Hacker");
+
+            Console.WriteLine(result);
+        }
+    }
+}

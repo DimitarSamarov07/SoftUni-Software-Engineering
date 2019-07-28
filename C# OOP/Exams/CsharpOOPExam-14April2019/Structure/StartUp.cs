0991ca56b3d36bc4bb673ec91e5207b873309728
DimitarSamarov07 <samarovdimitar@gmail.com>
2019-07-28 19:31:19 +0300

    Structure 50/50 points done!

--- C# OOP/Exams/CsharpOOPExam-14April2019/Structure/StartUp.cs
+++ C# OOP/Exams/CsharpOOPExam-14April2019/Structure/StartUp.cs
@@ -8,16 +8,7 @@ namespace MortalEngines
     {
         public static void Main()
         {
-            IFighter sth = new Fighter("PESHO", 12, 30);
-            ITank sth2 = new Tank("Gosho", 303, 30);
-            Pilot ivancho = new Pilot("Ivancho");
-            ivancho.AddMachine(sth2);
-            sth.Attack(sth2);
-            ivancho.AddMachine(sth);
-            Console.WriteLine(ivancho.Report());
-            sth.ToggleAggressiveMode();
-            sth2.ToggleDefenseMode();
-            Console.WriteLine(ivancho.Report());
+
         }
     }
 }

892aa24826135cfcb332ae3c4c5afb268d1091f4
Dimitar Samarov <samarovdimitar@gmail.com>
2019-09-26 11:57:45 +0300

    Code updated!!

--- C# DB/Databases Basics - MS SQL Server/Basic CRUD - Exercises/CreateViewEmployeesWithJobTitles.sql
+++ C# DB/Databases Basics - MS SQL Server/Basic CRUD - Exercises/CreateViewEmployeesWithJobTitles.sql
@@ -1,3 +1,3 @@
 CREATE VIEW V_EmployeeNameJobTitle AS
-SELECT FirstName+' '+IIF(MiddleName IS NULL, '', MiddleName)+' '+LastName AS 'Full Name',JobTitle
+SELECT CONCAT(FirstName,' ',MiddleName,' ',LastName)AS 'Full Name',JobTitle
 FROM Employees

db8a0381d2b28b1fd3682e2136fa85fc58ccf8f7
Dimitar Samarov <samarovdimitar@gmail.com>
2019-10-16 22:04:47 +0300

    17/22 problems done!!

---
+++ C# DB/Databases Basics - MS SQL Server/Database Programmability and Transactions - Exercises/17WithdrawMoneyProcedure.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE usp_WithdrawMoney(@accountId INT, @moneyAmount DECIMAL(18, 4))
+AS
+    BEGIN TRANSACTION
+    IF (@moneyAmount < 0)
+        BEGIN
+            RAISERROR ('Invalid money amount!!',16,1)
+        END
+UPDATE Accounts
+SET Balance-=@moneyAmount
+WHERE Id = @accountId
+    COMMIT

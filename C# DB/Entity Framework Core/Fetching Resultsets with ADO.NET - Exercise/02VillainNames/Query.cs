39b5c464e6ea29963a5c6daa1069886a7477dabc
Dimitar Samarov <samarovdimitar@gmail.com>
2019-10-30 19:23:39 +0200

    2/9 problems done!!

---
+++ C# DB/Entity Framework Core/Fetching Resultsets with ADO.NET - Exercise/02VillainNames/Query.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02VillainNames
+{
+    public static class Query
+    {
+        public const string GetVillainNames = @"SELECT Name, COUNT(MV.MinionId) AS [CountOfMinions]
+        FROM Villains V
+            LEFT JOIN MinionsVillains MV ON V.Id = MV.VillainId
+            GROUP BY Name
+        HAVING COUNT(MV.MinionId) > 3
+        ORDER BY CountOfMinions";
+    }
+}
---
+++ C# DB/Entity Framework Core/Fetching Resultsets with ADO.NET - Exercise/02VillainNames/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace _02VillainNames
+{
+    using System;
+    using _01InitialSetup;
+    using System.Data.SqlClient;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connectionMinions = new SqlConnection(Configuration.ConnectionStringMinionsDB);
+
+            connectionMinions.Open();
+
+            using (connectionMinions)
+            {
+                using SqlCommand command = new SqlCommand(Query.GetVillainNames, connectionMinions);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string villainName = (string)reader["Name"];
+                    int countOfMinions = (int)reader["CountOfMinions"];
+                    Console.WriteLine($"{villainName} - {countOfMinions}");
+                }
+            }
+        }
+    }
+}

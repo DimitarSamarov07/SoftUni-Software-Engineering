34fd6c0aa54e407fb218119eacafe7a89f555e99
Dimitar Samarov <samarovdimitar@gmail.com>
2019-10-30 19:23:03 +0200

    1/9 problems done!!

---
+++ C# DB/Entity Framework Core/Fetching Resultsets with ADO.NET - Exercise/01InitialSetup/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace _01InitialSetup
+{
+    using System.Data.SqlClient;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connectionMaster = new SqlConnection(Configuration.ConnectionStringMaster);
+           
+            SqlConnection connectionMinions = new SqlConnection(Configuration.ConnectionStringMinionsDB);
+            
+            connectionMaster.Open();
+            
+            using (connectionMaster)
+            {
+                using SqlCommand createDbSqlCommand = new SqlCommand(Configuration.CreateDatabase, connectionMaster);
+                
+                createDbSqlCommand.ExecuteNonQuery();
+               
+                connectionMinions.Open();
+                
+                using (connectionMinions)
+                {
+                    foreach (var createQuery in Configuration.CreateQueries)
+                    {
+                        using SqlCommand createTablesSqlCommand = new SqlCommand(createQuery, connectionMinions);
+                        
+                        createTablesSqlCommand.ExecuteNonQuery();
+                    }
+
+                    foreach (var insertQuery in Configuration.InsertQueries)
+                    {
+                        using SqlCommand insertIntoTables = new SqlCommand(insertQuery, connectionMinions);
+                        
+                        insertIntoTables.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+    }
+}

8a98f7f3015e3eb24b20306010a7e1ff45a54bdb
Dimitar Samarov <samarovdimitar@gmail.com>
2019-10-30 19:23:50 +0200

    3/9 problems done!!

---
+++ C# DB/Entity Framework Core/Fetching Resultsets with ADO.NET - Exercise/03MinionNames/Query.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03MinionNames
+{
+    public static class Query
+    {
+        public const string MinionsInfoQuery = @"SELECT M.Name, Age, ROW_NUMBER() OVER (ORDER BY M.Name) AS [RowNumber]
+                                             FROM Minions M
+                                                JOIN MinionsVillains MV ON M.Id = MV.MinionId
+                                                JOIN Villains V ON MV.VillainId = V.Id
+                                             WHERE V.Id = @Id
+                                             ORDER BY M.Name";
+
+        public const string VillainName = @"SELECT Name
+                                             FROM Villains V
+                                             WHERE Id = @Id";
+    }
+}
---
+++ C# DB/Entity Framework Core/Fetching Resultsets with ADO.NET - Exercise/03MinionNames/StartUp.cs
@@ -0,0 +1,57 @@
+﻿namespace _03MinionNames
+{
+    using System;
+    using System.Data.SqlClient;
+    using _01InitialSetup;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int villainId = int.Parse(Console.ReadLine());
+
+            SqlConnection minionsConnection = new SqlConnection(Configuration.ConnectionStringMinionsDB);
+
+            minionsConnection.Open();
+
+            using (minionsConnection)
+            {
+                using SqlCommand villainNameSqlCommand = new SqlCommand(Query.VillainName, minionsConnection);
+
+                villainNameSqlCommand.Parameters.AddWithValue("@Id", villainId);
+
+                string villainName = (string)villainNameSqlCommand.ExecuteScalar();
+
+                if (villainName == null)
+                {
+                    Console.WriteLine($"No villain with ID {villainId} exists in the database.");
+                    return;
+                }
+
+                using SqlCommand minionsQuery = new SqlCommand(Query.MinionsInfoQuery, minionsConnection);
+                minionsQuery.Parameters.AddWithValue("@Id", villainId);
+
+                SqlDataReader reader = minionsQuery.ExecuteReader();
+
+                using (reader)
+                {
+                    Console.WriteLine($"Villain: {villainName}");
+
+                    if (!reader.HasRows)
+                    {
+                        Console.WriteLine("(no minions)");
+                    }
+
+                    while (reader.Read())
+                    {
+                        long rowNumber = (long)reader["RowNumber"];
+                        string minionName = (string)reader["Name"];
+                        int minionAge = (int)reader["Age"];
+
+                        Console.WriteLine($"{rowNumber}. {minionName} {minionAge}");
+                    }
+                }
+            }
+        }
+    }
+}

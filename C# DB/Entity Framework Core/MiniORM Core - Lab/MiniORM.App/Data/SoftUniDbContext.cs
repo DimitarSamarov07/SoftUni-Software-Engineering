6ece4113106767f20809bb8afb2479e3e2d75720
Dimitar Samarov <samarovdimitar@gmail.com>
2019-11-01 10:37:47 +0200

    Homework - DONE!!!

---
+++ C# DB/Entity Framework Core/MiniORM Core - Lab/MiniORM.App/Data/SoftUniDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace MiniORM.App.Data
+{
+    using Entities;
+
+    public class SoftUniDbContext : DbContext
+    {
+        public SoftUniDbContext(string connectionString)
+            : base(connectionString)
+        {
+        }
+
+        public DbSet<Employees> Employees { get; }
+        public DbSet<Departments> Departments { get; }
+        public DbSet<Projects> Projects { get; }
+        public DbSet<EmployeesProjects> EmployeesProjects { get; }
+    }
+}
---
+++ C# DB/Entity Framework Core/MiniORM Core - Lab/MiniORM.App/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace MiniORM.App
+{
+    using System.Linq;
+    using Data;
+    using Data.Entities;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Server=.\SQLEXPRESS;Database=MiniORM;Integrated Security=true";
+            
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employees
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true
+            });
+
+            var employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+            context.SaveChanges();
+        }
+    }
+}

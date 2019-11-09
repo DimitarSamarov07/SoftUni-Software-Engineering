0b0683bd8399f5e96ee26b10a2755d6f0dad5e85
Dimitar Samarov <samarovdimitar@gmail.com>
2019-11-09 17:49:55 +0200

    Last test left!!

---
+++ C# DB/Entity Framework Core/Entity Relations - Exercise/StudentSystem/Data/Migrations/20191109124821_Initial1.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P01_StudentSystem.Data.Migrations
+{
+    public partial class Initial1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HomeworkSubmission_Courses_CourseId",
+                table: "HomeworkSubmission");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_HomeworkSubmission_Students_StudentId",
+                table: "HomeworkSubmission");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_HomeworkSubmission",
+                table: "HomeworkSubmission");
+
+            migrationBuilder.RenameTable(
+                name: "HomeworkSubmission",
+                newName: "HomeworkSubmissions");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_HomeworkSubmission_StudentId",
+                table: "HomeworkSubmissions",
+                newName: "IX_HomeworkSubmissions_StudentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_HomeworkSubmission_CourseId",
+                table: "HomeworkSubmissions",
+                newName: "IX_HomeworkSubmissions_CourseId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_HomeworkSubmissions",
+                table: "HomeworkSubmissions",
+                column: "HomeworkId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HomeworkSubmissions_Courses_CourseId",
+                table: "HomeworkSubmissions",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HomeworkSubmissions_Students_StudentId",
+                table: "HomeworkSubmissions",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "StudentId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HomeworkSubmissions_Courses_CourseId",
+                table: "HomeworkSubmissions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_HomeworkSubmissions_Students_StudentId",
+                table: "HomeworkSubmissions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_HomeworkSubmissions",
+                table: "HomeworkSubmissions");
+
+            migrationBuilder.RenameTable(
+                name: "HomeworkSubmissions",
+                newName: "HomeworkSubmission");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_HomeworkSubmissions_StudentId",
+                table: "HomeworkSubmission",
+                newName: "IX_HomeworkSubmission_StudentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_HomeworkSubmissions_CourseId",
+                table: "HomeworkSubmission",
+                newName: "IX_HomeworkSubmission_CourseId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_HomeworkSubmission",
+                table: "HomeworkSubmission",
+                column: "HomeworkId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HomeworkSubmission_Courses_CourseId",
+                table: "HomeworkSubmission",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HomeworkSubmission_Students_StudentId",
+                table: "HomeworkSubmission",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "StudentId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ C# DB/Entity Framework Core/Entity Relations - Exercise/StudentSystem/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace P01_StudentSystem
+{
+    using System;
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StudentSystemContext db = new StudentSystemContext();
+            db.Database.EnsureCreated();
+        }
+    }
+}

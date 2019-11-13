072b2ac0b2a27f45ab2fef3f81bfdc6523d02081
Dimitar Samarov <samarovdimitar@gmail.com>
2019-11-13 21:00:02 +0200

    1/15 problems done!!

--- C# DB/Entity Framework Core/Advanced Querying - Exercise/00BookShopDatabase/StartUp.cs
+++ C# DB/Entity Framework Core/Advanced Querying - Exercise/00BookShopDatabase/StartUp.cs
@@ -1,12 +1,38 @@
 ﻿namespace BookShop
 {
     using System;
+    using System.Linq;
+    using System.Text;
+    using Data;
+    using Models.Enums;
 
-    class StartUp
+    public class StartUp
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BookShopContext context = new BookShopContext();
+            Console.WriteLine(GetBooksByAgeRestriction(context, "teEn"));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var age = Enum.Parse<AgeRestriction>(command, true);
+
+            var books = context.Books
+                .Where(x => x.AgeRestriction == age)
+                .Select(x=>new
+                {
+                    BookTitle = x.Title
+                })
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var book in books.OrderBy(x=>x.BookTitle))
+            {
+                sb.AppendLine(book.BookTitle);
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }

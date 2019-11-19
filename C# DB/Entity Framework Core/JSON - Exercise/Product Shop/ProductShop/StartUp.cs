cebc8ec862aab94003a88d1851ab1d8d8d339c92
Dimitar Samarov <samarovdimitar@gmail.com>
2019-11-19 18:10:54 +0200

    1/19 problems done!!

---
+++ C# DB/Entity Framework Core/JSON - Exercise/Product Shop/ProductShop/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Xml.XPath;
+    using Newtonsoft.Json.Serialization;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            context.Database.EnsureCreated();
+
+            Console.WriteLine(GetUsersWithProducts(context));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<User[]>(inputJson);
+
+            context.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+        }
+

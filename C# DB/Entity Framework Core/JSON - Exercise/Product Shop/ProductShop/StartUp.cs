1f2c40e0e8512602019aff2724e842bb61cb6c91
Dimitar Samarov <samarovdimitar@gmail.com>
2019-11-19 18:13:02 +0200

    4/19 problems done!!

--- C# DB/Entity Framework Core/JSON - Exercise/Product Shop/ProductShop/StartUp.cs
+++ C# DB/Entity Framework Core/JSON - Exercise/Product Shop/ProductShop/StartUp.cs
@@ -54,3 +54,14 @@ namespace ProductShop
 
             return $"Successfully imported {categories.Length}";
         }
+
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
+        {
+            var categoryProducts = JsonConvert.DeserializeObject<CategoryProduct[]>(inputJson);
+
+            context.AddRange(categoryProducts);
+            context.SaveChanges();
+
+            return $"Successfully imported {categoryProducts.Length}";
+        }
+

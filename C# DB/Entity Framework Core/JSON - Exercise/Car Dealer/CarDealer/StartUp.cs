90bfc1f35523e54829a129adbc14d77135b3082e
Dimitar Samarov <samarovdimitar@gmail.com>
2019-11-20 19:47:13 +0200

    13/19 problems done!!

--- C# DB/Entity Framework Core/JSON - Exercise/Car Dealer/CarDealer/StartUp.cs
+++ C# DB/Entity Framework Core/JSON - Exercise/Car Dealer/CarDealer/StartUp.cs
@@ -97,3 +97,13 @@
             return $"Successfully imported {customers.Length}.";
         }
 
+        public static string ImportSales(CarDealerContext context, string inputJson)
+        {
+            var sales = JsonConvert.DeserializeObject<Sale[]>(inputJson);
+
+            context.AddRange(sales);
+            context.SaveChanges();
+
+            return $"Successfully imported {sales.Length}.";
+        }
+

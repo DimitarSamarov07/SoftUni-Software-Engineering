48363cc28acec6c3615aaca32e32d6e42e7e03c9
Dimitar Samarov <samarovdimitar@gmail.com>
2019-11-25 12:25:24 +0200

    1/3 problems done!!

---
+++ C# DB/Entity Framework Core/Design Patterns - Exercise/01. Prototype/SandwichMenu.cs
@@ -0,0 +1,17 @@
+﻿namespace _01._Prototype
+{
+    using System.Collections.Generic;
+
+    public class SandwichMenu
+    {
+        private Dictionary<string, SandwichPrototype> _sandwiches =
+            new Dictionary<string, SandwichPrototype>();
+
+        public SandwichPrototype this[string name]
+        {
+            get => this._sandwiches[name];
+            set => this._sandwiches.Add(name, value);
+        }
+
+    }
+}

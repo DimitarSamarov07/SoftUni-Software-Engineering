210c8b268ea87588600ad07bbaaf76eb36ca59f0
Dimitar Samarov <samarovdimitar@gmail.com>
2019-11-25 12:25:46 +0200

    2/3 problems done!!

---
+++ C# DB/Entity Framework Core/Design Patterns - Exercise/02. Composite/SingleGift.cs
@@ -0,0 +1,19 @@
+﻿namespace _02._Composite
+{
+    using System;
+
+    public class SingleGift:GiftBase
+    {
+        public SingleGift(string name, int price) 
+            : base(name, price)
+        {
+        }
+
+        public override int CalculateTotalPrice()
+        {
+            Console.WriteLine($"{name} with the price {this.price}");
+
+            return this.price;
+        }
+    }
+}

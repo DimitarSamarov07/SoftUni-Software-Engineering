e26f09cb4ff226e7595f3bc847c733c5cdac3127
DimitarSamarov07 <samarovdimitar@gmail.com>
2020-05-27 21:02:50 +0300

    6/11 problems done!

---
+++ JS Advanced/JS Advanced/02. Arrays/Exercise/06. Sort an Array by 2 Criteria.js
@@ -0,0 +1,9 @@
+sortBy2Criteria(['alpha',
+    'beta',
+    'gamma']
+)
+function sortBy2Criteria(inputArray) {
+    let sortedArray = inputArray.sort(((a, b) => a.length - b.length || a.localeCompare(b)))
+
+    console.log(sortedArray.join("\n"));
+}

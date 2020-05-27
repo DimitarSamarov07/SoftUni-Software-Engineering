9c11d74c738ee6c1f10a641c9ef449d3704131cc
DimitarSamarov07 <samarovdimitar@gmail.com>
2020-05-27 21:46:43 +0300

    7/11 problems done!

---
+++ JS Advanced/JS Advanced/02. Arrays/Exercise/07. Magic Matrices.js
@@ -0,0 +1,19 @@
+// [
+// [4, 5, 6],
+// [6, 5, 4],        an example of a ,,magical" array - the sums of the cells of every row and every column are equal.
+// [5, 5, 5]
+// ]
+
+function isArrayMagical(inputArray) {
+    let sumsSet = new Set;
+
+    for (let row = 0; row < inputArray.length; row++) {
+        sumsSet.add(inputArray[row].reduce((x, y) => x + y))
+        let colSum = 0;
+        for (let col = 0; col < inputArray[row].length; col++) {
+            colSum += inputArray[row][col];
+        }
+    }
+
+    console.log(sumsSet.size === 1);
+}

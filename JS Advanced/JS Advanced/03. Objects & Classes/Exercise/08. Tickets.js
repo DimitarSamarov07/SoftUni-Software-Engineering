31ad78498fa17a8d8d0257f7a3d74aa79c9e954c
DimitarSamarov07 <samarovdimitar@gmail.com>
2020-06-04 16:12:40 +0300

    Fix the non working status sort.

--- JS Advanced/JS Advanced/03. Objects & Classes/Exercise/08. Tickets.js
+++ JS Advanced/JS Advanced/03. Objects & Classes/Exercise/08. Tickets.js
@@ -16,9 +16,9 @@ function sortTickets(inputArray, criteria) {
         tickets.push(new Ticket(destination, price, status))
     })
 
-    criteria === "destination" ? tickets = tickets.sort((a,b) => a.destination.localeCompare(b.destination)) : "";
-    criteria === "price" ? tickets = tickets.sort((a,b) => a.price - b.price) : "";
-    criteria === "status" ? tickets = tickets.sort((a,b) => a.destination.localeCompare(b.destination)) : "";
+    criteria === "destination" ? tickets.sort((a, b) => a.destination.localeCompare(b.destination)) : "";
+    criteria === "price" ? tickets.sort((a, b) => a.price - b.price) : "";
+    criteria === "status" ? tickets.sort((a, b) => a.status.localeCompare(b.status)) : "";
 
     return tickets;
 }

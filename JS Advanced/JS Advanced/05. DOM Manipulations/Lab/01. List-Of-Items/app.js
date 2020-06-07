7069f3d0aac530ddbfb1071705b38bc3cf4c9d2d
DimitarSamarov07 <samarovdimitar@gmail.com>
2020-06-07 19:32:59 +0300

    1/7 problems done!

---
+++ JS Advanced/JS Advanced/05. DOM Manipulations/Lab/01. List-Of-Items/app.js
@@ -0,0 +1,15 @@
+function addItem() {
+  let itemsList = document.getElementById("items");
+  let textInput = document.getElementById("newItemText");
+
+  let textInputValue = textInput.value;
+
+  if (!textInputValue) {
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.innerText = textInputValue;
+
+  itemsList.appendChild(li);
+}

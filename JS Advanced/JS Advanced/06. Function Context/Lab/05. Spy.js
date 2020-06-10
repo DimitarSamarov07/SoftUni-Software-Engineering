2085d2934b03177810705d7818478cc82a8c7b2a
DimitarSamarov07 <samarovdimitar@gmail.com>
2020-06-10 22:50:16 +0300

    Lab done!

---
+++ JS Advanced/JS Advanced/06. Function Context/Lab/05. Spy.js
@@ -0,0 +1,12 @@
+function Spy(obj, method)
+{
+    const spy = {count: 0}
+    let originFunc = obj[method]
+
+    obj[method] = function (...args) {
+        this.count++;
+        return originFunc.apply(obj, args)
+    }.bind(spy);
+
+    return spy;
+}

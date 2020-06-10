24624edf1dbbd59ae23de00dfd2cbbc177c29921
DimitarSamarov07 <samarovdimitar@gmail.com>
2020-06-10 21:03:41 +0300

    1/5 problems done!

---
+++ JS Advanced/JS Advanced/06. Function Context/Lab/01. Area and Volume Calculator.js
@@ -0,0 +1,21 @@
+function solve(area, vol, input) {
+    let objects = JSON.parse(input);
+
+    function calc(obj) {
+        let areaObj = Math.abs(area.call(obj))
+        let volumeObj = Math.abs(vol.call(obj))
+
+        return {area: areaObj, volume: volumeObj}
+    }
+
+    return objects.map(calc)
+}
+
+function area() {
+    return this.x * this.y;
+}
+
+function vol() {
+    return this.x * this.y * this.z;
+
+}

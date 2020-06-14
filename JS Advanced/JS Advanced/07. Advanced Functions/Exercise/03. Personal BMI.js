55281171be8f35efb0237d3b54deff0c12d2305a
DimitarSamarov07 <samarovdimitar@gmail.com>
2020-06-14 17:47:03 +0300

    3/7 problems done!

---
+++ JS Advanced/JS Advanced/07. Advanced Functions/Exercise/03. Personal BMI.js
@@ -0,0 +1,33 @@
+function personalBMI(name, age, weight, height) {
+    let bmi = weight / Math.pow(height / 100, 2);
+    let status = getStatus(bmi);
+
+    let objToReturn = {
+        name: name,
+        personalInfo: {
+            age: age,
+            weight: Math.round(weight),
+            height: Math.round(height)
+        },
+        BMI: Math.round(bmi),
+        status: status
+    }
+
+    status === "obese" ? objToReturn["recommendation"] = "admission required" : "";
+
+    return objToReturn;
+
+    function getStatus(bmi) {
+        if (bmi < 18.5) {
+            return "underweight";
+        } else if (bmi < 25) {
+            return "normal";
+        } else if (bmi < 30) {
+            return "overweight";
+        } else if (bmi >= 30) {
+            return "obese";
+        }
+    }
+}
+
+console.log(personalBMI("Honey Boo Boo", 9, 57, 137))

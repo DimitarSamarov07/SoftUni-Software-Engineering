ca76fc19e0d5fa3bd883d1256159343c0244afc3
DimitarSamarov07 <samarovdimitar@gmail.com>
2020-06-19 21:29:56 +0300

    Lab done...finally!

---
+++ JS Advanced/JS Advanced/08. Object Composition/Lab/05. Sum.js
@@ -0,0 +1,24 @@
+function solution() {
+
+    let first, second, result;
+
+    function init(selector1, selector2, resultSelector) {
+        first = document.querySelector(selector1);
+        second = document.querySelector(selector2);
+        result = document.querySelector(resultSelector);
+    }
+
+    function add() {
+        result.value = Number(first.value) + Number(second.value);
+    }
+
+    function subtract() {
+        result.value = Number(first.value) - Number(second.value);
+    }
+
+    return {
+        init,
+        add,
+        subtract
+    }
+}
